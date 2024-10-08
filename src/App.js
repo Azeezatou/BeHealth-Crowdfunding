04e8a110e6c738209b44063fc550ebd4bc33b905
Abdulqadir Azeezat <Azeezatou>
2024-10-08 17:54:45 +0100

    wallet integration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import { WalletAdapterNetwork, WalletNotConnectedError } from '@solana/wallet-adapter-base';
+import { ConnectionProvider, WalletProvider, useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { Button } from '@solana/wallet-adapter-react-ui/lib/types/Button';
+
+import {
+    LedgerWalletAdapter,
+    PhantomWalletAdapter,
+    SolflareWalletAdapter
+} from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl, Transaction, SystemProgram, Keypair, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
+
 import Campaigns from './Components/Campaigns';
 import HeroSection from './Components/HeroSection';
 import NavBar from './Components/NavBar';
@@ -11,11 +23,7 @@ function App() {
         <NavBar />
         <HeroSection/>
         <Campaigns/>
-        <Link to="/donate">Donate</Link>
       </div>
-      <Routes>
-        {/* <Route path="/donate" element={<DonationPage/>}/> */}
-      </Routes>
     </Router>
     
   );
---
+++ src/Components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./NavBar";
+import HeroSection from "./HeroSection";
+
+function Main() {
+    return (
+        <div>
+
+            <Navbar/>
+            <HeroSection/>
+            <Campaigns/>
+        </div>
+    )
+}
